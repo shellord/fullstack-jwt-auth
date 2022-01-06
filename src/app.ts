04eda38dd504ca7ba728dbd11d431a8b6cabb530
saheenshoukath <saheenshoukath@gmail.com>
2022-01-06 19:33:30 +0530

    added morgan and removed env file from git

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import morgan from 'morgan'
 
 dotenv.config()
 
 const app = express()
 const PORT = process.env.PORT || 3000
+
+app.use(morgan('dev'))
 
 app.listen(PORT, () => {
   console.log('Server is running on port: ' + PORT)
